ff5d810b69e02c01dfe322863f4a1b3d29a883e7
LAPTOP-BNJPDH74\ikimo <ikimono-3@keio.jp>
2023-01-19 17:45:31 +0900

    fix judge

--- Assets/RWIS/JudgeUp.cs
+++ Assets/RWIS/JudgeUp.cs
@@ -9,15 +9,15 @@ public class JudgeUp : MonoBehaviour
     private float ellapsedTime = 0.0f;
     private Vector3 displayPos;
 
-#if UNITY_ANDROID && !UNITY_EDITOR
-    public static AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-    public static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-    public static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
-#else
-    public static AndroidJavaClass unityPlayer;
-    public static AndroidJavaObject currentActivity;
-    public static AndroidJavaObject vibrator;
-#endif
+// #if UNITY_ANDROID && !UNITY_EDITOR
+//     public static AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+//     public static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+//     public static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+// #else
+//     public static AndroidJavaClass unityPlayer;
+//     public static AndroidJavaObject currentActivity;
+//     public static AndroidJavaObject vibrator;
+// #endif
 
     public AudioClip sound1;
     AudioSource audioSource;
@@ -68,25 +68,25 @@ public class JudgeUp : MonoBehaviour
             isDisplay = true;
 
             audioSource.PlayOneShot(sound1);
-            Vibrate(1000);
+            // Vibrate(1000);
         }
     }
 
-    public static void Vibrate(long milliseconds)
-    {
-        if (isAndroid()) {
-            vibrator.Call("vibrate", milliseconds);
-        }
-        else {
-            Handheld.Vibrate();
-        }
-    }
-    private static bool isAndroid()
-    {
-#if UNITY_ANDROID && !UNITY_EDITOR
-        return true;
-#else
-        return false;
-#endif
-    }
+//     public static void Vibrate(long milliseconds)
+//     {
+//         if (isAndroid()) {
+//             vibrator.Call("vibrate", milliseconds);
+//         }
+//         else {
+//             Handheld.Vibrate();
+//         }
+//     }
+//     private static bool isAndroid()
+//     {
+// #if UNITY_ANDROID && !UNITY_EDITOR
+//         return true;
+// #else
+//         return false;
+// #endif
+//     }
 }
